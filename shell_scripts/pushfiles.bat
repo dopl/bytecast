fd2a34d262f9c84771cb37409b0b836a5a6ff47d
root <root@dnandola.syr.edu>
2013-02-16 14:09:07 -0500

    Shell Scripts

---
+++ shell_scripts/pushfiles.bat
@@ -0,0 +1,16 @@
+cd ~/code/bytecast-fsys
+git remote rm origin
+git remote add origin https://github.com/dopl/bytecast-fsys
+git push origin master
+
+git remote rm origin
+git remote add origin https://github.com/dopl/bytecast-amd64
+git push origin master
+
+git remote rm origin
+git remote add origin https://github.com/dopl/bytecast-jimple
+git push origin master
+
+git remote rm origin
+git remote add origin https://github.com/dopl/bytecast-runtime
+git push origin master
